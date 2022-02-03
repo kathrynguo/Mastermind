90e96252bd5a88a4435d4ba038cff71500d6e0e9
kathrynguo <59924894+kathrynguo@users.noreply.github.com>
2022-02-03 18:13:31 -0500

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -9,7 +9,7 @@ public class Mastermind {
 
 
 	//setting code (4 digit array)
-		//psuedo guess with ints
+	//psuedo guess with ints
 		private int[] intCode = new int[4];
 
 		//real guess with letters 
@@ -18,11 +18,23 @@ public class Mastermind {
 	//building code; WITH REPEATS
 	public void setCode() {
 		//random number to each index in code 
-		for (int x = 0; x < 4; x++) {
-			intCode[x] = (int)(Math.random()*7 + 1); 
-			//System.out.print(guess[x] + "");
-		}
+		
+		boolean rp = true;
+		for (int x = 0; x < 4; x++) { 
+		do {
+				intCode[x] = (int)(Math.random()*7 + 1); 
+				//System.out.print(guess[x] + "");
+					if (x == 0 || intCode[x] != intCode [x-1]){
+						//if its equal to the one before it
+						//has not repeated, so break out of loop
+						rp = false; 
+					}
+					else {
+						rp = true;  
+					}
+			}while(rp == true); 
 		//System.out.print("/n"); 
+		}
 		
 			
 		//setting letters in code based on numbers in intCode
@@ -59,8 +71,19 @@ public class Mastermind {
 		}
 		 
 		//print code
-		System.out.println(code); 
+		//System.out.println(code); 
 		
+		//in setCode 
+		//l1; l2; l3; l4; 
+		//check that string does not contain l2, l3, l4 already within the substring you've already checked
+		//for loop to find numbers of repeats of letters
+
+			
+	}
+	
+	public void printCode() {
+		System.out.print("CODE: "); 
+		System.out.print(code);
 	}
 	
 	
@@ -84,6 +107,8 @@ public class Mastermind {
 		for (int i = 0; i < 4; i++) {
 			board.get(a).add("[ ]");
 		}
+		//System.out.println("new guess array"); 
+		a = a + 2; 
 		
 		//add new array for check; odd numbered
 		board.add(new ArrayList<String>() );
@@ -93,30 +118,33 @@ public class Mastermind {
 		for (int y = 0; y < 4; y++) {
 			board.get(b).add("( )");  
 		}
-		
+		//System.out.println(b); 
+		b = b + 2; 
 	//}
 	
 	}
 	
+	private int a1 = 0; //for even number array guesses
+	private int b1 = 1; //for odd number array checks
 	//last in tester class
 	public void printBoard() {
 		
 		//printing out guess board
 		for (int c = 0; c < 4; c ++){ 
-		System.out.print(board.get(a).get(c) + " ");
+		System.out.print(board.get(a1).get(c) + " ");
 		}
 		System.out.println("\n"); 
 		//adds 2 after printing 
-		a = a + 2; 
+		a1 = a1 + 2; 
 		
 		//printing out check array
-		System.out.print(board.get(b).get(0) + " "); 
-		System.out.print(board.get(b).get(1) + " "); 
+		System.out.print(board.get(b1).get(0) + " "); 
+		System.out.print(board.get(b1).get(1) + " "); 
 		System.out.println(""); 
-		System.out.print(board.get(b).get(2) + " "); 
-		System.out.print(board.get(b).get(3) + " "); 
-		//adds 2 after printing 
-		b = b + 2; 
+		System.out.print(board.get(b1).get(2) + " "); 
+		System.out.print(board.get(b1).get(3) + " "); 
+		//adds 2 after printing 1
+		b1 = b1 + 2; 
 		
 		System.out.println("\n");
 		
@@ -127,12 +155,10 @@ public class Mastermind {
 	private int ck = 1; //for populating check
 	//boolean to make sure there are no replacements when checking the setting key 
 	private boolean occ=true; //true = occupied 
+	public String uGuess; 
 	
-	public void guessAndCheck() { //check is not working
-		
-		//string for user guess
-		String uGuess; 
-		
+	public void guess() { //check is not working
+
 		//getting guess from user
 		Scanner s1 = new Scanner(System.in);
 		System.out.print("Guess: ");
@@ -151,20 +177,13 @@ public class Mastermind {
 		
 		//checking
 		//int c = 0; //for else if 
+		//System.out.println(ck); 
+	}
+	
+	
+	public boolean check(boolean win){
 		
-		/*
-		//checking for repeat letters in code
-		int rep = 0;
-		int k = 0; 
-				//checking how many times it contains that letter
-				for (int h = 0; h < 4; h++) {
-					if (code.get(h).equals(uGuess.substring(k, k + 1))) {
-						rep = rep + 1;
-						k = k + 1;  
-					}
-				}
-			System.out.println(rep + " reps");
-			*/
+		win = false; 
 		
 		//circulate through all four
 		for (int i = 0; i < 4; i++) {
@@ -189,36 +208,72 @@ public class Mastermind {
 				} while(occ == true); //DOUBLE EQUAL SIGN 
 			}//close if 
 			//only matches color
-			else if (code.contains(uGuess.substring(i, i + 1))){ //can lend itself to false doubles; must fix 
+			else if (code.contains(uGuess.substring(i, i + 1))){ 
 				//System.out.println("WHITE PIN");
+				
+			
+				//checking how many times it contains that letter in the code
+				int apC = 0;
+				for (int h = 0; h < 4; h++) {
+					if ((code.get(h)).equals(uGuess.substring(i, i + 1))) {
+						apC = apC + 1;  //appears once
+					}
+				}
+				//System.out.println(apC + " appearances in code");
+				//if number of times in guess is greater than number of reps in code
+				
+				//checking for repeats in guess
+				int apG = 0; 
+				for (int w = 0; w < 4; w++) {
+					//if letter 1 equals letter 2, 3, 4, and so on
+					//need to change so that it doesnt double count the last letter
+					if ((uGuess.substring(w, w+1)).equals(uGuess.substring(i, i + 1))) {
+						apG = apG + 1;  //appearances in guess
+					}
+				}
+				//System.out.println(apG + " appearances in guess");
+			
+
+			
 				do {
 					int d = (int)(Math.random()*4);
 					String x = board.get(ck).get(d); 
-					if (x.equals("( )") ) {
+					//if it appears more in the guess than it does in the code, you have double counted
+					if (x.equals("( )") && (apC >= apG)){ 
 						board.get(ck).set(d,"(W)");  
 						//System.out.println("w pin set");
 						occ = false; 
 						
 					}
-					else {
+					else if (apG > apC) { //if appears more in the guess than it does in the code
+						//no population
+						occ = false; 
+					}
+					else { //occupied, get out of loop 
 						occ = true; 
 						//System.out.println(d + "loop"); 
 					}
-				} while(occ == true); //DOUBLE EQUAL SIGN
-				//breaks out when the number of times you printed becomes euqal to the number of times the letter is in the code 
+				} while(occ == true); //DOUBLE EQUAL SIGN 
 				
 			}//close else if
 			} //close for loop 
 			
+			//checking to see if you win 
+			if (board.get(ck).contains("( )") || board.get(ck).contains ("(W)")) { //means its not all black
+				win = false;
+				ck = ck + 2;
+				//System.out.print("lose"); 
+			}
+			else {
+				win = true; 
+				ck = ck + 2;
+				//System.out.print("win");
+			}
+			return win; 
+			 
 			
-			//add 2 to check array index
-			ck = ck + 2;
-		} //close guessAndCheck method
+		} 
+	
 		//System.out.print(win + "\n"); 
 	}
-
-//in setCode 
-//l1; l2; l3; l4; 
-//check that string does not contain l2, l3, l4 already within the substring you've already checked
-//for loop to find numbers of repeats of letters
 
